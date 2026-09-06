2757a94749a11911c5d5050a4b30ed50fd2af4d4
Cherenity <50301874+Cherenity@users.noreply.github.com>
2026-09-06 15:53:44 +0300

    add add, delete, edit book

--- bookstore/src/main/java/harjoitus/bookstore/web/BookController.java
+++ bookstore/src/main/java/harjoitus/bookstore/web/BookController.java
@@ -3,8 +3,11 @@ package harjoitus.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import harjoitus.bookstore.domain.Book;
 import harjoitus.bookstore.domain.BookRepository;
 
 
@@ -34,6 +37,31 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
-  
+
+    @GetMapping("/addBook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/saveBook")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/deleteBook/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        bookRepository.deleteById(id);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/editBook/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        Book book = bookRepository.findById(id).orElse(null);
+        model.addAttribute("book", book);
+        return "addbook";
+    }
+    
 
 }
