2b9c1d68aeed88334e2cc852737a1d4ac2628f7a
Cherenity <50301874+Cherenity@users.noreply.github.com>
2026-09-06 13:25:59 +0300

    changes to book controller

--- bookstore/src/main/java/harjoitus/bookstore/web/BookController.java
+++ bookstore/src/main/java/harjoitus/bookstore/web/BookController.java
@@ -1,22 +1,39 @@
 package harjoitus.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import harjoitus.bookstore.domain.BookRepository;
+
 
 @Controller
-@ResponseBody
 public class BookController {
 
-  @GetMapping("/index")
-  public String index() {
-      return "This is the index page of the bookstore application.";
-  }
+    private final BookRepository bookRepository;
+    
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "This is the index page of the bookstore application.";
+    }
+    
+    @GetMapping("/*")
+    @ResponseBody
+    public String fallback() {
+        return "Empty page. Please check the URL.";
+    }
+
+    @GetMapping("/books")
+    public String showAllBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
   
-  @GetMapping("/*")
-  public String fallback() {
-      return "Empty page. Please check the URL.";
-  }
 
 }
